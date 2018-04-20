0360ecf1a75180538c5b0060bb91dcfb723fcff3
Navy <navych@126.com>
2018-04-20 12:59:05 +0800

    init

---
+++ src/main/java/com/navy/a301/A301Application.java
@@ -0,0 +1,12 @@
+package com.navy.a301;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A301Application.class, args);
+	}
+}
---
+++ src/test/java/com/navy/a301/A301ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.navy.a301;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class A301ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
